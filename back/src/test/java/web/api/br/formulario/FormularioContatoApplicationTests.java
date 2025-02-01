c4cc1b87894c557213a1552f6c696c3eaa1e50ef
Dissoares <diegosoaresyt@gmail.com>
2025-02-01 19:44:22 -0300

    adicionando webapi spring com java 21

---
+++ back/src/test/java/web/api/br/formulario/FormularioContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package web.api.br.formulario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

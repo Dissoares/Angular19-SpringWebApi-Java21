f0ad869baf6bb61762ff89b2bb8d835380c511b3
Dissoares <diegosoaresyt@gmail.com>
2025-02-11 21:23:45 -0300

    corrigindo rotas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { FormularioComponent } from './components/formulario-modulo/formulario.component'; 
+import { ListagemComponent } from './components/listagem-modulo/listagem.component'; 
 import { RotasEnum } from './core/enums/rotas.enum';
 
 export const routes: Routes = [
   {
-    path: 'formulario',
-    loadComponent: () =>
-      import('./layout/home/home.component').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'listagem',
-    loadComponent: () =>
-      import('./layout/home/home.component').then((m) => m.HomeComponent),
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'formulario', component: FormularioComponent },
+      { path: 'listagem', component: ListagemComponent },
+      { path: '', redirectTo: RotasEnum.FORMULARIO, pathMatch: 'full' },
+    ],
   },
   { path: '**', redirectTo: RotasEnum.FORMULARIO },
 ];
--- front/src/app/layout/home/home.component.ts
+++ front/src/app/layout/home/home.component.ts
@@ -1,15 +1,13 @@
 import { FormularioComponent } from '../../components/formulario-modulo/formulario.component';
+import { ListagemComponent } from '../../components/listagem-modulo/listagem.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Router, RouterModule } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { ListagemComponent } from '../../components/listagem-modulo/listagem.component';
-import { NavigationEnd, Router } from '@angular/router';
-import { RotasEnum } from '../../core/enums/rotas.enum';
-import { filter } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -23,7 +21,8 @@ import { CommonModule } from '@angular/common';
     SidebarComponent,
     FooterComponent,
     HeaderComponent,
-    CommonModule
+    RouterModule,
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
@@ -31,11 +30,9 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent implements OnInit {
   public sidebarAberta: boolean = true;
   public ehDispositivoMobile: boolean = false;
-  public rotaAtualEhListagem: Boolean = false;
 
   constructor(
-    private breakpointObserver: BreakpointObserver,
-    private router: Router
+    private breakpointObserver: BreakpointObserver
   ) {}
 
   ngOnInit() {
@@ -44,20 +41,6 @@ export class HomeComponent implements OnInit {
 
   public ativarSidebar() {
     this.sidebarAberta = !this.sidebarAberta;
-  }
-
-  public verificarRotaAtual() {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd)) 
-      .subscribe((event: NavigationEnd) => {
-        if(event.urlAfterRedirects === RotasEnum.LISTAGEM){
-          this.rotaAtualEhListagem = true 
-        } else{
-          this.rotaAtualEhListagem = false;
-        } 
-        console.log("💡 EhListagem:", event.urlAfterRedirects)
-        console.log("💡 this.rotaAtualEhListagem:", this.rotaAtualEhListagem)
-      });
   }
 
   public verificarDispositivoAtualEhMobile() {

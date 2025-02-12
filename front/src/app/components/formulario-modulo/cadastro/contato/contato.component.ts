bd701a6c14ddc885357f6c9efe2dad744fa28415
Dissoares <diegosoaresyt@gmail.com>
2025-02-12 00:20:48 -0300

    finalizando dados de contato

--- front/src/app/components/formulario-modulo/cadastro/contato/contato.component.ts
+++ front/src/app/components/formulario-modulo/cadastro/contato/contato.component.ts
@@ -1,6 +1,13 @@
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { TipoTelefoneEnum } from '../../../../core/enums';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,19 +18,20 @@ import { CommonModule } from '@angular/common';
   selector: 'app-contato',
   standalone: true,
   imports: [
-    MatCardModule,
-    CommonModule,
-    MatIconModule,
     MatFormFieldModule,
-    MatInputModule,
+    ReactiveFormsModule,
     MatSelectModule,
-    ReactiveFormsModule
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
   ],
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.scss'],
 })
 export class ContatoComponent implements OnInit {
   public formulario!: FormGroup;
+  public tipoTelefoneEnum = TipoTelefoneEnum.getAllValues();
 
   constructor(private form: FormBuilder) {}
 

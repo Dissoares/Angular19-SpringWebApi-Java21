ca7e4cc250a3666988538251a85e63cdc5a53ee2
Dissoares <diegosoaresyt@gmail.com>
2025-02-22 00:53:34 -0300

    adicionando máscara para tipos de telefone

--- front/src/app/pages/formulario-modulo/dados-contato/dados-contato.component.ts
+++ front/src/app/pages/formulario-modulo/dados-contato/dados-contato.component.ts
@@ -1,14 +1,15 @@
 import { ErrosCamposFormularioComponent } from '../../../core/components/erros-campos-formulario/erros-campos-formulario.component';
 import { validadorBase } from '../../../core/components/formulario-base/formulario-base.component';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { TipoTelefoneEnum } from '../../../core/enums';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 @Component({
   selector: 'app-contato',
   standalone: true,
@@ -16,17 +17,20 @@ import { CommonModule } from '@angular/common';
     ErrosCamposFormularioComponent,
     ReactiveFormsModule,
     MatFormFieldModule,
+    NgxMaskDirective,
     MatSelectModule,
     MatInputModule,
     MatCardModule,
     MatIconModule,
     CommonModule,
   ],
+  providers: [provideNgxMask()],
   templateUrl: './dados-contato.component.html',
   styleUrls: ['./dados-contato.component.scss'],
 })
 export class DadosContatoComponent extends validadorBase implements OnInit {
   public tipoTelefoneEnum = TipoTelefoneEnum.getAllValues();
+  public mascaraTelefone: string = '';
 
   constructor() {
     super(new FormBuilder());
@@ -45,11 +49,24 @@ export class DadosContatoComponent extends validadorBase implements OnInit {
         [this.ehCampoObrigatorio(), this.validarEmail(), this.confirmarEmail()],
       ],
       tipoTelefone: [null, this.ehCampoObrigatorio()],
-      numeroTelefone: [null, this.ehCampoObrigatorio()],
+      numeroTelefone: [
+        { value: null, disabled: true }, 
+        [
+          this.ehCampoObrigatorio(),
+          Validators.minLength(11)
+        ],
+      ],
     });
   }
 
   public validarConfirmacaoEmail(campo: string) {
     this.validarCampo(campo);
+  }
+
+  public alterarMascaraTelefone(evento: MatSelectChange) {
+    this.ativarCampo('numeroTelefone')
+    evento.value === TipoTelefoneEnum.CELULAR.id
+      ? (this.mascaraTelefone = '(99)99999-9999')
+      : (this.mascaraTelefone = '(99)9999-9999');
   }
 }

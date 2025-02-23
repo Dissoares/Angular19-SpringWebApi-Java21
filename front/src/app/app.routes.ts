3d13444415000b37bfcce39d6121a95696f92c2a
Dissoares <diegosoaresyt@gmail.com>
2025-02-23 00:32:50 -0300

    configurando rotas sem lazy loading

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,23 @@
+import { AlunosFormularioComponent } from './modulos/modulos-aluno/alunos-formulario/alunos-formulario.component';
+import { AlunosListagemComponent } from './modulos/modulos-aluno/alunos-listagem/alunos-listagem.component';
+import { ModulosAlunoComponent } from './modulos/modulos-aluno/modulos-aluno.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pagina-inicial',
+    component: PaginaInicialComponent,
+    children: [
+      {
+        path: 'alunos',
+        component: ModulosAlunoComponent,
+        children: [
+          { path: 'listagem', component: AlunosListagemComponent },
+          { path: 'formulario', component: AlunosFormularioComponent },
+        ],
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'pagina-inicial' },
+];

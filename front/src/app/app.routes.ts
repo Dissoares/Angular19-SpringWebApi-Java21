6c0a0149e5512af70f8e6c509ab9f3086af0bef7
Dissoares <diegosoaresyt@gmail.com>
2025-02-23 00:39:19 -0300

    configurando rotas com Lazy Loading para otimizar o carregamento dos componentes da aplicação

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,23 +1,41 @@
-import { AlunosFormularioComponent } from './modulos/modulos-aluno/alunos-formulario/alunos-formulario.component';
-import { AlunosListagemComponent } from './modulos/modulos-aluno/alunos-listagem/alunos-listagem.component';
-import { ModulosAlunoComponent } from './modulos/modulos-aluno/modulos-aluno.component';
-import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
     path: 'pagina-inicial',
-    component: PaginaInicialComponent,
+    loadComponent: () =>
+      import('./pagina-inicial/pagina-inicial.component').then(
+        (c) => c.PaginaInicialComponent
+      ),
     children: [
       {
         path: 'alunos',
-        component: ModulosAlunoComponent,
+        loadComponent: () =>
+          import('./modulos/modulos-aluno/modulos-aluno.component').then(
+            (c) => c.ModulosAlunoComponent
+          ),
         children: [
-          { path: 'listagem', component: AlunosListagemComponent },
-          { path: 'formulario', component: AlunosFormularioComponent },
+          {
+            path: 'listagem',
+            loadComponent: () =>
+              import(
+                './modulos/modulos-aluno/alunos-listagem/alunos-listagem.component'
+              ).then((c) => c.AlunosListagemComponent),
+          },
+          {
+            path: 'formulario',
+            loadComponent: () =>
+              import(
+                './modulos/modulos-aluno/alunos-formulario/alunos-formulario.component'
+              ).then((c) => c.AlunosFormularioComponent),
+          },
         ],
       },
     ],
   },
-  { path: '**', redirectTo: 'pagina-inicial' },
+  {
+    path: '',
+    redirectTo: 'pagina-inicial',
+    pathMatch: 'full',
+  },
 ];
--- front/src/app/modulos/modulos-aluno/modulos-aluno.component.ts
+++ front/src/app/modulos/modulos-aluno/modulos-aluno.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-modulos-aluno',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
   templateUrl: './modulos-aluno.component.html',
-  styleUrls: ['./modulos-aluno.component.scss']
+  styleUrls: ['./modulos-aluno.component.scss'],
 })
 export class ModulosAlunoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

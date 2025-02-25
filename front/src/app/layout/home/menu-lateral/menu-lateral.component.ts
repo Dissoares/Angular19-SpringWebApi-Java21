bbebfee2419a4289a73f0c47215cf736c0305c97
Dissoares <diegosoaresyt@gmail.com>
2025-02-25 00:37:56 -0300

    atualiznado menu

--- front/src/app/layout/home/menu-lateral/menu-lateral.component.ts
+++ front/src/app/layout/home/menu-lateral/menu-lateral.component.ts
@@ -4,25 +4,30 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RotasEnum } from 'app/core/enums/rotas.enum';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatIconModule, MatListModule],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule,
+  ],
   templateUrl: './menu-lateral.component.html',
   styleUrls: ['./menu-lateral.component.scss'],
 })
 export class MenuLateralComponent implements OnInit {
+  public botao: string = 'formulario';
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
-  public navegarParaFormulario() {
-    this.router.navigate(['sistema/alunos/formulario']);
-  }
-
-  public navegarParaListagem() {
-    this.router.navigate(['sistema/alunos/listagem']);
+  public ativarBotaoENavegar(rotaBotao: string) {
+    this.router.navigate([`sistema/alunos/${rotaBotao}`]);
+    this.botao = rotaBotao;
   }
 }

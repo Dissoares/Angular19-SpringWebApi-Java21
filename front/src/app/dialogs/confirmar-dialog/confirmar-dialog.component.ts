db14d936ddcab175f125fe2fa7a1a1b9bb2edc22
Dissoares <diegosoaresyt@gmail.com>
2025-03-01 23:44:45 -0300

    redirecionando e passando id pela rota para a listagem ao salva aluno

--- front/src/app/dialogs/confirmar-dialog/confirmar-dialog.component.ts
+++ front/src/app/dialogs/confirmar-dialog/confirmar-dialog.component.ts
@@ -2,38 +2,59 @@ import {
   MAT_DIALOG_DATA,
   MatDialogModule,
   MatDialogRef,
+  MatDialog,
 } from '@angular/material/dialog';
+import {
+  Component,
+  inject,
+  OnInit,
+  Inject,
+} from '@angular/core';
 import { SnackBarPersonalizadoService } from '../../core/services/index.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AlunosService } from '../../core/services/alunos.service';
-import { Component, inject, OnInit, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { Aluno } from 'app/core/models';
-
+import { Rotas } from 'app/core/enums';
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-confirmar-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    CommonModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './confirmar-dialog.component.html',
   styleUrls: ['./confirmar-dialog.component.scss'],
 })
 export class ConfirmarDialogComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<ConfirmarDialogComponent>);
+  readonly spinnerDialog = inject(MatDialog);
   private snackBarService = inject(SnackBarPersonalizadoService);
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public Aluno: Aluno,
-    private alunosService: AlunosService
+    private alunosService: AlunosService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
 
   public salvarDados(): void {
     this.alunosService.salvarDadosPessoais(this.Aluno).subscribe({
-      next: () => {
+      next: (aluno: Aluno) => {
         this.snackBarService.abrirSnackBar('Cadastro concuído!.', 'Sucesso');
-        this.fecharDialogo();
+        timer(1000).subscribe(() => {
+          this.fecharDialogo();
+        });
+
+        this.redirecionarParaOutraRota(aluno);
       },
       error: (erro: HttpErrorResponse) => {
         const tipoErro = erro.error?.message;
@@ -50,6 +71,16 @@ export class ConfirmarDialogComponent implements OnInit {
           return;
         }
       },
+    });
+  }
+
+  public redirecionarParaOutraRota(aluno: Aluno): void {
+    timer(5000).subscribe(() => {
+      this.router.navigate([Rotas.SISTEMA.alunos.LISTAGEM], {
+        queryParams: {
+          idAluno: aluno.dadosPessoais.id,
+        },
+      });
     });
   }
 

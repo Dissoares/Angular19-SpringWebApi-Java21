a479b3d7d174bf60baa51240094f643df3925dc3
Dissoares <diegosoaresyt@gmail.com>
2025-03-02 16:20:50 -0300

    concatenando endereço

--- front/src/app/modulos/alunos-modulos/alunos-listagem/alunos-listagem.component.ts
+++ front/src/app/modulos/alunos-modulos/alunos-listagem/alunos-listagem.component.ts
@@ -6,10 +6,11 @@ import {
 import { AlunosFiltroComponent } from './alunos-filtro/alunos-filtro.component';
 import { getPaginatorPortugues } from 'app/shared/config/paginator-portugues';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { ColunasTabelaAlunos, DadosEndereco, Aluno } from 'app/core/models';
 import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { FormatarDataPtbrPipe } from 'app/shared/formatar-data-ptbr.pipe';
 import { AlunosService } from 'app/core/services/alunos.service';
-import { Aluno, ColunasTabelaAlunos } from 'app/core/models';
+import { TipoSexoEnum, TipoTelefoneEnum } from 'app/core/enums';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -17,7 +18,6 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { TipoSexoEnum } from 'app/core/enums';
 
 @Component({
   selector: 'app-alunos-listagem',
@@ -78,6 +78,23 @@ export class AlunosListagemComponent implements AfterViewInit, OnInit {
   }
 
   public getDescricaoTipoSexo(sexo: number): string {
+    if (!sexo) return '';
     return TipoSexoEnum.getByCodigo(sexo)?.descricao || '';
+  }
+
+  public getDescricaoTipoTelefone(tipo: number): string {
+    if (!tipo) return '';
+    return TipoTelefoneEnum.getByCodigo(tipo)?.descricao || '';
+  }
+
+  public retonaEnderecoConcatenado(endereco: DadosEndereco): string {
+    const concatenado: string[] = [];
+    if (endereco.rua && endereco.numero)
+      concatenado.push(`${endereco.rua} Nº${endereco.numero} - `);
+    if (endereco.cep) concatenado.push(`CEP: ${endereco.cep}`);
+    if (endereco.bairro) concatenado.push(`, ${endereco.bairro}`);
+    if (endereco.cidade) concatenado.push(`, ${endereco.cidade}`);
+    if (endereco.estado) concatenado.push(`/${endereco.estado}`);
+    return concatenado.join('') || '';
   }
 }

8482619f8788e098c94639cf410bed6da903a06b
Dissoares <diegosoaresyt@gmail.com>
2025-03-04 00:17:48 -0300

    movendo modulo usuario

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -35,43 +35,6 @@ export const routes: Routes = [
       },
     ],
   },
-  {
-    path: 'sistema',
-    loadComponent: () =>
-      import('./layout/home/conteudo/conteudo.component').then(
-        (c) => c.ConteudoComponent
-      ),
-    children: [
-      {
-        path: 'usuario',
-        loadComponent: () =>
-          import('./modulos/usuarios-modulos/usuarios-modulos.component').then(
-            (c) => c.UsuariosModulosComponent
-          ),
-        children: [
-          {
-            path: 'formulario',
-            loadComponent: () =>
-              import(
-                './modulos/usuarios-modulos/usuarios-formulario/usuarios-formulario.component'
-              ).then((c) => c.UsuariosFormularioComponent),
-          },
-          {
-            path: 'listagem',
-            loadComponent: () =>
-              import(
-                './modulos/usuarios-modulos/usuarios-listagem/usuarios-listagem.component'
-              ).then((c) => c.UsuariosListagemComponent),
-          },
-        ],
-      },
-      {
-        path: '**',
-        redirectTo: 'usuarios',
-        pathMatch: 'full',
-      },
-    ],
-  },
 
   {
     path: '**',
